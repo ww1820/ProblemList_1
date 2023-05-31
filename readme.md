bfbf7c9084b015bca60b972fe0b485f21b651014
wwu <ww1820@qq.com>
2023-05-31 21:59:08 +0800

    TEX Quotes

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 OJ题单链接：https://vjudge.net/article/45
+
+删除out文件夹：
+
+```bash
+rm -rf out */out
+```
--- 数组和字符串/TEX Quotes.cpp
+++ 数组和字符串/TEX Quotes.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 /*
 input: 
-"To be or not to be," quoth the Bard, "that is the question".
-The programming contestant replied: "I must disagree. To `C' or not to `C', that is The Question!"
+"To be or not to be," quoth the Bard, "that
+is the question".
+The programming contestant replied: "I must disagree.
+To `C' or not to `C', that is The Question!"
 
 output: 
-``To be or not to be,'' quoth the Bard, ``that is the question''.
-The programming contestant replied: ``I must disagree. To `C' or not to `C', that is The Question!'
+``To be or not to be,'' quoth the Bard, ``that
+is the question''.
+The programming contestant replied: ``I must disagree.
+To `C' or not to `C', that is The Question!'
 */
 
 int main()
 {
     freopen("../input.in", "r", stdin);
     // code
-    
+    char c;
+    bool flag = true;
 
+    while ((c = getchar()) != EOF)
+    {
+        if(c == '\"')
+        {
+            printf(flag ? "``" : "''");
+            flag = !flag;
+        }
+        else
+        {
+            printf("%c", c);
+        }
+    }
     return 0;
 }
