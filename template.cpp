06124a46b7cc664b14cceee447311f90f646b9f8
wwu <ww1820@qq.com>
2023-05-31 20:03:23 +0800

    first commit

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    input: 
+
+    output: 
+*/
+
+int main()
+{
+    freopen("../input.in", "r", stdin);
+    // code
+    
+    return 0;
+}
---
+++ 数组和字符串/TEX Quotes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+input: 
+"To be or not to be," quoth the Bard, "that is the question".
+The programming contestant replied: "I must disagree. To `C' or not to `C', that is The Question!"
+
+output: 
+``To be or not to be,'' quoth the Bard, ``that is the question''.
+The programming contestant replied: ``I must disagree. To `C' or not to `C', that is The Question!'
+*/
+
+int main()
+{
+    freopen("../input.in", "r", stdin);
+    // code
+    
+
+    return 0;
+}
